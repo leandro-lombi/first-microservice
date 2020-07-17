5c6a1f50208df099169431ffedb4888d0a25b5c9
Leandro Lombi <lombi.leandro@gmail.com>
2020-07-17 13:47:25 -0300

    Inclusao do script de persistencia da tabela de produto

---
+++ src/main/resources/db/migration/V3__Insert_produto.sql
@@ -0,0 +1,14 @@
+INSERT INTO PRODUTO(descricao, sku, peso, largura, altura, profundidade, fabricante, preco)
+VALUES ('Pneu para carro', 'PNEU-PIR-55X55X22', 7.35, 55.0, 55.0, 22.0, 'Pirelli', 89.95);
+
+INSERT INTO PRODUTO(descricao, sku, peso, largura, altura, profundidade, fabricante, preco)
+VALUES ('Pneu para caminhonete', 'PNEU-BRID-90X90X38.7', 15.23, 90.0, 90.0, 38.7, 'Bridgestone', 115.75);
+
+INSERT INTO PRODUTO(descricao, sku, peso, largura, altura, profundidade, fabricante, preco)
+VALUES ('Volante para o carro Gol', 'VOL-SNUT-GOL-35X35X3', 3.0, 35.0, 35.0, 3.0, 'Snut', 55.45);
+
+INSERT INTO PRODUTO(descricao, sku, peso, largura, altura, profundidade, fabricante, preco)
+VALUES ('Volante para o carro Civic', 'VOL-SURF-CIVIC-36X36X3.5', 3.0, 36.0, 36.0, 3.5, 'Surf', 74.68);
+
+INSERT INTO PRODUTO(descricao, sku, peso, largura, altura, profundidade, fabricante, preco)
+VALUES ('Banco esportivo', 'BANCO-CARRARO-67X78.6X1.1', 26.15, 67.0, 78.6, 1.10, 'Carraro', 233.75);
